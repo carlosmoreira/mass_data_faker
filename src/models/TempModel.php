081dc36c9bdc2dce66e5a90b74303ba3b3164b03
Carlos Moreira <moreira.carlos09@gmail.com>
2018-11-09 15:14:52 -0500

    created new model to be a temp placeholder as the eloquent model we will use to save the data.

---
+++ src/models/TempModel.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This class will be used a placeholder to connect to eloquent.
+ *
+ * The table name will be overridden in a private method before attempting a db save
+ *
+ */
+
+namespace src\models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class TempModel extends Model
+{
+
+    public function overideTableName($tableName)
+    {
+        $this->table = $tableName;
+    }
+
+}
