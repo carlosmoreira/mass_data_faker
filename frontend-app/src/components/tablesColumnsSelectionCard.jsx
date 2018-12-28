8ab6a75bed74594ef1ceb101bbe56944ff21c97b
Carlos Moreira <moreira.carlos09@gmail.com>
2018-12-28 16:25:51 -0500

    parsed table and columns data in component.

--- frontend-app/src/components/tablesColumnsSelectionCard.jsx
+++ frontend-app/src/components/tablesColumnsSelectionCard.jsx
@@ -12,59 +12,27 @@ class TablesColumnsSelectionCard extends Component {
         <div className="card-header text-center">Tables</div>
         <div className="card-body">
           <div className="list-group list-group-root">
-            <a
-              href="#item-2"
-              className="list-group-item"
-              data-toggle="collapse"
-            >
-              <i className="glyphicon glyphicon-chevron-right" />
-              Table 1 (8)
-            </a>
-            <div className="list-group collapse" id="item-2">
-              <div className="list-group-item pt-0 pb-0">
-                <input type="checkbox" id="col1" />{" "}
-                <label htmlFor="col1">Column 1 (PK)</label>
-              </div>
-
-              <div className="list-group-item pt-0 pb-0">
-                <input type="checkbox" id="col2" />{" "}
-                <label htmlFor="col2">
-                  Column 2 <span className="text-info">(Nullable)</span>
-                </label>
-              </div>
-
-              <div className="list-group-item pt-0 pb-0">
-                <input type="checkbox" id="col3" />{" "}
-                <label htmlFor="col3">
-                  Column 3 <span className="text-danger">(Required)</span>
-                </label>
-              </div>
-
-              <div className="list-group-item pt-0 pb-0">
-                <input type="checkbox" id="col4" />{" "}
-                <label htmlFor="col4">Column 4</label>
-              </div>
-
-              <div className="list-group-item pt-0 pb-0">
-                <input type="checkbox" id="col5" />{" "}
-                <label htmlFor="col5">Column 5</label>
-              </div>
-
-              <div className="list-group-item pt-0 pb-0">
-                <input type="checkbox" id="col6" />{" "}
-                <label htmlFor="col6">Column 6</label>
-              </div>
-
-              <div className="list-group-item pt-0 pb-0">
-                <input type="checkbox" id="col7" />{" "}
-                <label htmlFor="col7">Column 7</label>
-              </div>
-
-              <div className="list-group-item pt-0 pb-0">
-                <input type="checkbox" id="col8" />{" "}
-                <label htmlFor="col8">Column 8</label>
-              </div>
-            </div>
+            {tables.map(table => (
+              <React.Fragment>
+                <a
+                  key={table.name}
+                  href={`#${table.name}`}
+                  className="list-group-item"
+                  data-toggle="collapse"
+                >
+                  <i className="glyphicon glyphicon-chevron-right" />
+                  {table.name} ({table.columns.length})
+                </a>
+                <div className="list-group collapse" id={`${table.name}`}>
+                  {table.columns.map(column => (
+                    <div className="list-group-item pt-0 pb-0">
+                      <input type="checkbox" id="col1" />
+                      <label htmlFor="col1"> {column.name}</label>
+                    </div>
+                  ))}
+                </div>
+              </React.Fragment>
+            ))}
           </div>
         </div>
       </div>
