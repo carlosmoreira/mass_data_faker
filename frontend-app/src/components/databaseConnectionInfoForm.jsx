1901bde4a4270c057358f021a083fcdc14ef28bc
Carlos Moreira <moreira.carlos09@gmail.com>
2018-12-31 01:36:33 -0500

    connected db connection form with formik library

--- frontend-app/src/components/databaseConnectionInfoForm.jsx
+++ frontend-app/src/components/databaseConnectionInfoForm.jsx
@@ -1,51 +1,110 @@
 import React, { Component } from "react";
+import { Formik, FormikProps, Form, Field } from "formik";
 
 class DatabaseConnectionInformationForm extends Component {
-  state = {};
+  //state = {};
+
+  handleSubmit = (values, { props = this.props, setSubmitting }) => {
+    //process form submission here
+    //done submitting, set submitting to false
+    console.log("values", values);
+    setSubmitting(false);
+    return;
+  };
+
   render() {
     return (
-      <form action="">
-        <div className="form-group row">
-          <div className="col">
-            <div className="card">
-              <div className="card-header text-center">
-                Database Connection Information
-              </div>
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-md-2">
-                    <label htmlFor="db_type">Database Type</label>
-                    <select
-                      className="form-control"
-                      name="db_type"
-                      id="db_type"
-                    >
-                      <option value="mysql">MySql</option>
-                      <option value="sql">SQL</option>
-                    </select>
+      <Formik
+        initialValues={{
+          db_type: "",
+          host: "",
+          username: "",
+          password: "",
+          database: ""
+        }}
+        validate={values => {
+          let errors = [];
+          // if(!values.email_address)
+          //   errors.push()
+          //check if my values have errors
+          return errors;
+        }}
+        onSubmit={this.handleSubmit}
+        render={({ errors, status, touched, isSubmitting }) => (
+          <Form>
+            <div className="form-group row">
+              <div className="col">
+                <div className="card">
+                  <div className="card-header text-center">
+                    Database Connection Information
                   </div>
-                  <div className="col-md-3">
-                    <label htmlFor="host">Host</label>
-                    <input className="form-control" type="text" id="host" />
-                  </div>
-                  <div className="col-md-2">
-                    <label htmlFor="username">Username</label>
-                    <input className="form-control" type="text" id="username" />
-                  </div>
-                  <div className="col-md-2">
-                    <label htmlFor="password">Password</label>
-                    <input className="form-control" type="text" id="password" />
-                  </div>
-                  <div className="col-md-3">
-                    <label htmlFor="database">Database</label>
-                    <input className="form-control" type="text" id="database" />
+                  <div className="card-body">
+                    <div className="row">
+                      <div className="col-md-2">
+                        <label htmlFor="db_type">Database Type</label>
+                        <Field
+                          className="form-control"
+                          name="db_type"
+                          component="select"
+                          placeholder="Database Type"
+                        >
+                          <option value="mysql">MySql</option>
+                          <option value="sqlsvr">MSSQL</option>
+                        </Field>
+                      </div>
+                      <div className="col-md-2">
+                        <label htmlFor="host">Host</label>
+                        <Field
+                          type="text"
+                          className="form-control"
+                          name="host"
+                        />
+                        {/* {errors.email && touched.email && <div>{errors.email}</div>} */}
+                      </div>
+                      <div className="col-md-2">
+                        <label htmlFor="username">Username</label>
+                        <Field
+                          type="text"
+                          className="form-control"
+                          name="username"
+                        />
+                        {/* {errors.email && touched.email && <div>{errors.email}</div>} */}
+                      </div>
+                      <div className="col-md-2">
+                        <label htmlFor="password">Password</label>
+                        <Field
+                          type="text"
+                          className="form-control"
+                          name="password"
+                        />
+                        {/* {errors.email && touched.email && <div>{errors.email}</div>} */}
+                      </div>
+                      <div className="col-md-2">
+                        <label htmlFor="database">Database</label>
+                        <Field
+                          type="text"
+                          className="form-control"
+                          name="database"
+                        />
+                        {/* {errors.email && touched.email && <div>{errors.email}</div>} */}
+                      </div>
+                      <div className="col-md-2 pt-4">
+                        <button
+                          className="btn btn-primary"
+                          type="submit"
+                          disabled={isSubmitting}
+                        >
+                          Submit
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </form>
+          </Form>
+        )}
+      />
     );
   }
 }
